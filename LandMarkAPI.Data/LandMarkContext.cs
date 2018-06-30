473ac770510d8a0427907d54a8f6fd5d38d48d56
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-06-30 12:30:52 +0200

    Added to db context

--- LandMarkAPI.Data/LandMarkContext.cs
+++ LandMarkAPI.Data/LandMarkContext.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LandMarkAPI.Domain.Entities.Auth;
+using LandMarkAPI.Domain.Entities.Person;
 using Microsoft.EntityFrameworkCore;
 
 namespace LandMarkAPI.Data
 {
     public class LandMarkContext :DbContext
     {
-		
+	    public LandMarkContext(DbContextOptions<LandMarkContext> options) : base(options){}
+
+		public DbSet<User> Users { get; set; }
+	    public DbSet<OAuth> OAuths { get; set; }
     }
 }
