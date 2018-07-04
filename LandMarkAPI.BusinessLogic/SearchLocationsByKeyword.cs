05e1e42381f2d2cccde242367de794cc0da6502d
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-07-04 20:49:28 +0200

    Adding logic to start retrieving results from Flickr api

---
+++ LandMarkAPI.BusinessLogic/SearchLocationsByKeyword.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LandMarkAPI.Authentication;
+using LandMarkAPI.Domain.Models.OAuth;
+
+namespace LandMarkAPI.BusinessLogic
+{
+    public class SearchLocationsByKeyword
+    {
+	    private readonly OAuthParamsRequestToken _flickr;
+
+	    public SearchLocationsByKeyword(OAuthParamsRequestToken flickr)
+	    {
+			var client = new OAuthClient(flickr).GetClient();
+		}
+
+		public string FindLocationByKeyword(string where)
+	    {
+		    var auth = new OAuthClient(_flickr).GetClient();
+			
+
+		    return "";
+	    }
+    }
+}
---
+++ LandMarkApi/Controllers/ListLocationsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LandMarkApi.Controllers
+{
+    public class ListLocationsController : Controller
+    {
+	    public List<int> ListLocations(string where)
+	    {
+
+
+		    return new List<int>();
+	    }
+    }
+}
