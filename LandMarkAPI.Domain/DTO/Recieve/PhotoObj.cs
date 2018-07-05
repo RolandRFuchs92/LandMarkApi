770a986cc0d74ac2c75822ea77355cb3d096dcad
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-07-05 23:37:12 +0200

    Added DTOs for Photos

---
+++ LandMarkAPI.Domain/DTO/Recieve/PhotoObj.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LandMarkAPI.Domain.Entities.Flickr;
+
+namespace LandMarkAPI.Domain.DTO.Recieve
+{
+	public class Photos
+	{
+		public PhotoObj photos { get; set; }
+		public string stat { get; set; }
+	}
+
+    public class PhotoObj
+    {
+	    public int page { get; set; }
+	    public int pages { get; set; }
+	    public int perpage { get; set; }
+	    public int total { get; set; }
+	    public List<Photo> Photo { get; set; }
+
+    }
+}
