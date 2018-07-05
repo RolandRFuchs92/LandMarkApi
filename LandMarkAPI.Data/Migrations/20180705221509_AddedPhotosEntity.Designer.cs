5cc2cc3fe56bffe6be8dfc1e49440725557279db
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-07-06 00:15:39 +0200

    Added migration for photo details

--- LandMarkAPI.Data/Migrations/20180705221509_AddedPhotosEntity.Designer.cs
+++ LandMarkAPI.Data/Migrations/20180705221509_AddedPhotosEntity.Designer.cs
@@ -12,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.Internal;
 namespace LandMarkAPI.Data.Migrations
 {
     [DbContext(typeof(LandMarkContext))]
-    partial class LandMarkContextModelSnapshot : ModelSnapshot
+    [Migration("20180705221509_AddedPhotosEntity")]
+    partial class AddedPhotosEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ LandMarkAPI.Data/Migrations/20180705221509_AddedPhotosEntity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LandMarkAPI.Data.Migrations
+{
+    public partial class AddedPhotosEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Photos",
+                columns: table => new
+                {
+                    PhotoId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    id = table.Column<long>(nullable: false),
+                    owner = table.Column<string>(nullable: true),
+                    secret = table.Column<string>(nullable: true),
+                    server = table.Column<int>(nullable: false),
+                    farm = table.Column<int>(nullable: false),
+                    title = table.Column<string>(nullable: true),
+                    ispublic = table.Column<bool>(nullable: false),
+                    isfriend = table.Column<bool>(nullable: false),
+                    isfamily = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Photos", x => x.PhotoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Photos");
+        }
+    }
+}
